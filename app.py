67165064464223570c1de2cc6ec0d84e21895db8
deltea <thcheetah777@gmail.com>
2024-12-27 22:13:52 -0700

    yay! listing files in tracks box (finally)

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from pathlib import Path
-from typing import Iterable
+from typing import Iterable, List
 
-from textual.app import App, ComposeResult
+from textual.app import App, ComposeResult, RenderResult
 from textual.binding import Binding
+from textual.reactive import reactive
 from textual.screen import Screen
 from textual.widgets import Footer, Static, ProgressBar, ListView, ListItem, Label
+from textual.widget import Widget
+
+
+# ---------- Widgets ----------
+class FolderItem(ListItem):
+  def __init__(self, path: Path, *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+    self.path = path
+
 
 class FolderTree(ListView):
-  path: str | Path
-
   def __init__(self, title: str = "", path: str = "", *args, **kwargs) -> None:
     super().__init__(*args, **kwargs)
     self.border_title = title
@@ -16,7 +24,7 @@ class FolderTree(ListView):
 
   def compose(self):
     for path in self.get_paths():
-      yield ListItem(Label(path.name))
+      yield FolderItem(path, Label(path.name))
 
   def get_paths(self) -> Iterable[Path]:
     return self.filter_paths(Path(self.path).iterdir())
@@ -25,26 +33,36 @@ class FolderTree(ListView):
     return [path for path in paths if path.is_dir() and not path.name.startswith(".")]
 
 class Tracks(ListView):
-  def __init__(self, title: str = "", *args, **kwargs) -> None:
+  tracks: List[Path] = reactive([])
+
+  def __init__(self, title: str = "", tracks: List[Path] = [], *args, **kwargs) -> None:
     super().__init__(*args, **kwargs)
     self.border_title = title
+    self.tracks = tracks
+
+  def watch_tracks(self, tracks: List[Path]) -> None:
+    self.clear()
+    self.refresh()
+    for i, path in enumerate(tracks, 1):
+      self.append(ListItem(
+        Label(f"[b]{i}[/b]"),
+        Label(path.name.removesuffix(path.suffix), classes="track-name"),
+        classes="track"
+      ))
 
 class PlayBar(Static):
   def __init__(self, *args, **kwargs) -> None:
     super().__init__(*args, **kwargs)
 
-class NowPlaying(Screen):
-  BINDINGS = [
-    ("n", "app.pop_screen", "Library"),
-  ]
+class NowPlaying(ListView):
+  def __init__(self, title: str = "", *args, **kwargs) -> None:
+    super().__init__(*args, **kwargs)
+    self.border_title = title
 
-  def compose(self) -> ComposeResult:
-    yield Label("Now playing")
-    yield Footer()
 
-class LollipopApp(App):
-  CSS_PATH = "style.tcss"
-  SCREENS = {"now_playing": NowPlaying}
+# ---------- Library Screen ----------
+
+class LibraryScreen(Screen):
   BINDINGS = [
     Binding("left,j", "seek(-5)", "seek backwards", show=False),
     Binding("right,l", "seek(5)", "seek forwards", show=False),
@@ -52,31 +70,16 @@ class LollipopApp(App):
 
     Binding("a", "add_to_queue", "add to queue"),
     Binding("p", "play_next", "play next"),
-    Binding("s", "push_screen('now_playing')", "show now playing"),
   ]
 
-  tracks: Iterable[Path] = []
-
-  def on_mount(self) -> None:
-    self.theme = "gruvbox"
-    self.title = "lollipop"
+  tracks: List[Path] = reactive([])
 
   def compose(self) -> ComposeResult:
-    self.tracks = self.update_tracks()
-
     yield Footer()
 
     with Static(classes="main"):
-      # Replace the Static widgets with Box widgets
-      # with Box("[b]Directory[/b]", classes="sidebar"):
-      yield FolderTree("[b]directory[/b]", "/Users/leo/Music", classes="sidebar")
-      with Tracks("[b]tracks[/b]", classes="tracks"):
-        index = 1
-        for path in self.tracks:
-          with ListItem(classes="track"):
-            yield Label(f"[b]{str(index)}[/b]")
-            yield Label(path.name.removesuffix(path.suffix), classes="track-name")
-          index += 1
+      yield FolderTree("[b]directory[/b]", "/Users/leo/Music", classes="folder-tree")
+      yield Tracks("[b]tracks[/b]", self.tracks, classes="tracks")
 
       with PlayBar():
         with Static(classes="playing-info"):
@@ -89,8 +92,15 @@ class LollipopApp(App):
           yield Label("shuffle")
           yield Label("repeat")
 
-  def update_tracks(self):
-    return Path("/Users/leo/Music/😈 aggressive phonk/").iterdir()
+  def update_tracks(self, path: str = "/Users/leo/Music/🌃 future funky city/"):
+    self.tracks = list(Path(path).iterdir())
+    self.query_one(Tracks).tracks = self.tracks
+    print(self.tracks)
+
+  def on_list_view_highlighted(self, message: ListView.Highlighted) -> None:
+    if message.list_view is self.query_one(FolderTree):
+      print(f"Highlighted: {message.item.path}")
+      self.update_tracks(message.item.path)
 
   def action_seek(self, time: float):
     self.query_one(ProgressBar).advance(time)
@@ -103,6 +113,38 @@ class LollipopApp(App):
 
   def action_play_next(self):
     self.notify("playing next")
+
+
+# ---------- Now Playing Screen ----------
+
+class NowPlayingScreen(Screen):
+  def compose(self) -> ComposeResult:
+    yield Footer()
+
+    with NowPlaying("[b]now playing[/b]", classes="box"):
+      yield ListItem(Label("Priestess"))
+      yield ListItem(Label("Aggressive Phonk"))
+      yield ListItem(Label("2:02"))
+
+
+# ---------- Main App ----------
+
+class LollipopApp(App):
+  CSS_PATH = "style.tcss"
+  MODES = {
+    "library": LibraryScreen,
+    "now_playing": NowPlayingScreen,
+  }
+  BINDINGS = [
+    Binding("1", "switch_mode('library')", "library", show=False),
+    Binding("2", "switch_mode('now_playing')", "now playing", show=False),
+  ]
+
+  def on_mount(self) -> None:
+    self.theme = "gruvbox"
+    self.title = "lollipop"
+    self.switch_mode("library")
+
 
 if __name__ == "__main__":
   app = LollipopApp()
